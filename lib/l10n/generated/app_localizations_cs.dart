60c13432609f7acc4e7d669414f73a48f696691d
petsob33 <petsob33@gmail.com>
2026-08-04 16:08:40 +0200

    feat(l10n): scaffold flutter_localizations + ARB pipeline

---
+++ lib/l10n/generated/app_localizations_cs.dart
@@ -0,0 +1,25 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Czech (`cs`).
+class AppLocalizationsCs extends AppLocalizations {
+  AppLocalizationsCs([String locale = 'cs']) : super(locale);
+
+  @override
+  String get commonCancel => 'Zrušit';
+
+  @override
+  String get commonDelete => 'Smazat';
+
+  @override
+  String get commonEdit => 'Upravit';
+
+  @override
+  String get commonConfirm => 'Potvrdit';
+
+  @override
+  String get commonSave => 'Uložit';
+}
